6f1bf4efc5f48748693aa0dd218d0c05008d7199
JuanDv <juanperdomobp@unicauca.edu.co>
2025-06-10 17:46:19 -0500

    APICLIENT Y SESSIONCONTEXT (Acceso mediante la apigateway)

--- FrontApp/FrontApp/src/main/java/com/mycompany/gestionproyectosacademicos/dto/ProjectDTO.java
+++ FrontApp/FrontApp/src/main/java/com/mycompany/gestionproyectosacademicos/dto/ProjectDTO.java
@@ -5,6 +5,7 @@ package com.mycompany.gestionproyectosacademicos.dto;
  * @author jpala
  */
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mycompany.gestionproyectosacademicos.client.CompanyAPIClient;
 import java.time.LocalDate;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -13,6 +14,7 @@ public class ProjectDTO {
     private int id;
     private String description;
     private String summary;
+    private String companyId;
     private String companyName;
     private String status;
     
@@ -20,21 +22,27 @@ public class ProjectDTO {
     public ProjectDTO() {
     }
     
-    public ProjectDTO(String name, int id, String description, String summary, String companyName, String status) {
+    public ProjectDTO(String name, int id, String description, String summary, String companyId, String companyName, String status) {
         this.name = name;
         this.id = id;
         this.description = description;
         this.summary = summary;
+        this.companyId = companyId;
         this.companyName = companyName;
         this.status = status;
     }
 
-    // Getters (y setters, si lo necesitas)
+    // Getters 
     public String getName() { return name; }
     public int getId() { return id; }
     public String getDescription() { return description; }
     public String getSummary() { return summary; }
-    public String getCompanyName() { return companyName; }
+    public String getCompanyId() {return companyId; }
+    public String getCompanyName() {
+        String companyName = null;
+        companyName = CompanyAPIClient.getCompanyNameById(companyId);
+        System.out.println("Nombre de la empresa recuperado: " + companyName);
+        return companyName; }
     public String getStatus() { return status; }
 }
 
